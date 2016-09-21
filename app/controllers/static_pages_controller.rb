a96a062926e84d7c6c42753d3a8d4ac47f9a2964
Izzy Matos <israeldmatos@gmail.com>
2016-09-21 08:49:06 -0700

    Adds static pages controller and maps out basic routes

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,35 @@
+get '/' do
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/schedule' do
+  erb :schedule
+end
+
+get '/classes' do
+  erb :classes
+end
+
+get 'ketogenics' do
+  erb :ketogenics
+end
+
+get 'blog' do 
+  erb :blog
+end
+
+get 'training' do
+  erb :training
+end 
+
+get 'contact' do
+  erb :contact
+end
+
+get 'store' do
+  erb :store
+end
