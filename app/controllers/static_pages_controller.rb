b8fba22fac3a5136846b55cef752242174144909
Izzy Matos <israeldmatos@gmail.com>
2016-09-21 20:25:18 -0700

    Fixes routes for static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,35 +1,35 @@
 get '/' do
-  erb :index
+  erb :'static_pages/index'
 end
 
 get '/about' do
-  erb :about
+  erb :'static_pages/about'
 end
 
 get '/schedule' do
-  erb :schedule
+  erb :'static_pages/schedule'
 end
 
 get '/classes' do
-  erb :classes
+  erb :'static_pages/classes'
 end
 
-get 'ketogenics' do
-  erb :ketogenics
+get '/ketogenics' do
+  erb :'static_pages/ketogenics'
 end
 
-get 'blog' do 
-  erb :blog
+get '/blog' do
+  erb :'static_pages/blog'
 end
 
-get 'training' do
-  erb :training
+get '/training' do
+  erb :'static_pages/training'
 end 
 
-get 'contact' do
-  erb :contact
+get '/contact' do
+  erb :'static_pages/contact'
 end
 
-get 'store' do
-  erb :store
+get '/store' do
+  erb :'static_pages/store'
 end
