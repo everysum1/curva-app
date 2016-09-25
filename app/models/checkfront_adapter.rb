b45e3aa0c463bca83983b68d0682e359d826ffad
Izzy Matos <israeldmatos@gmail.com>
2016-09-25 16:52:00 -0700

    Adds endpoint method to get all bookings

--- app/models/checkfront_adapter.rb
+++ app/models/checkfront_adapter.rb
@@ -4,17 +4,21 @@ class CheckfrontAdapter
   base_uri 'https://israelitech.checkfront.com/api/3.0'
 
   def initialize(args = {})
-    @username = args[:username]
-    @password = args[:password]
+    # @username = args[:username]
+    # @password = args[:password]
     @auth = { username: ENV['CHECKFRONT_KEY'], password: ENV['CHECKFRONT_SECRET'] }
-    test_stuff
+    # test_stuff
   end
 
-  def test_stuff
-    @github.test_endpoint("/")
-  end
+  # def test_stuff
+  #   @github.test_endpoint("/")
+  # end
 
   def test_endpoint(endpoint)
     self.class.get(endpoint, basic_auth: @auth)
+  end
+
+  def get_bookings
+    test_endpoint('/booking', basic_auth: @auth)["booking/index"]
   end
 end
